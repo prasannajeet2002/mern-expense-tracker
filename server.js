72327bfe887647df7fe8974989002326a8cc49ba
prasannajeet2002 <prasannajeetpradhan20@outlook.com>
2022-06-20 22:04:04 +0530

    small change

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use('/api/transactions' , transactions)
 
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV == 'production'){
     app.use(express.static('client/build'))
 
     app.get('*' , (req ,res) => res.sendFile(path.resolve(__dirname , 'client' , 'build', 'index.html')))
