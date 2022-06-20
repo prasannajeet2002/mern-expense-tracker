bf93f28a9347371d3347861ddd89c39bebda4386
prasannajeet2002 <prasannajeetpradhan20@outlook.com>
2022-06-21 00:13:57 +0530

    too many chnages

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb://localhost:27017/expense-tracker-react",
+      "mongodb+srv://babapadu:babapadu@cluster0.qkmr5.mongodb.net/expense-tracker-react",
       { useNewUrlParser: true, useUnifiedTopology: true }
     );
 
